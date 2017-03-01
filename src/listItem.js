418b13727ad8d55ba1662792075f1fe55b712ee0
Lukasz Gandecki <lukaszgandecki@Lukaszs-MacBook-Pro.local>
2017-03-01 14:56:20 +0000

    star wars 1

--- src/listItem.js
+++ src/listItem.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 export default class ListItem extends React.Component {
     render() {
-        return <li>Name: {this.props.name} - Location: {this.props.location}</li>
+        return <li>Planet: {this.props.name} - Location: {this.props.location}</li>
     }
 };
+
+// AIzaSyCv6f1yvgbhzkOgArqNn3LvGryg7Wxp-Xs
